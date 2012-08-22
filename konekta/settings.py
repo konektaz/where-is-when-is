4b5bdb9a5f4a0de85e36738dc092793e0c2ce87d
PA Parent <paparent@paparent.me>
2012-08-22 19:23:55 -0400

    Move home view to core app

--- konekta/settings.py
+++ konekta/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
 
     'olwidget',
 
+    'core',
     'world',
 )
 
--- konekta/urls.py
+++ konekta/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'konekta.views.home', name='home'),
+    url(r'^$', 'core.views.home', name='home'),
     # url(r'^konekta/', include('konekta.foo.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
