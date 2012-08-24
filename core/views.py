3b98684f49a5f8ef563be0542c5ae6ac9e5af91b
PA Parent <paparent@paparent.me>
2012-08-24 16:49:46 -0400

    add map to zone page and remove homepage one

--- core/views.py
+++ core/views.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from olwidget.widgets import InfoMap
-
-from world.models import WorldBorder
 
 
 def home(request):
-    worldborders = WorldBorder.objects.all()
-
-    this_map = InfoMap([(wb.poly_simplify, wb.name_1) for wb in worldborders])
-
-    return render(request, 'home.html', {'map': this_map})
+    return render(request, 'home.html')
--- world/views.py
+++ world/views.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from olwidget.widgets import InfoMap
+
+from models import Zone
 
 
 def zone_detail(request, slug):
+    zone = get_object_or_404(Zone, slug=slug)
+
+    worldborders = zone.worldborder_set.all()
+
+    this_map = InfoMap([(wb.poly_simplify, wb.name_1) for wb in worldborders])
+
+    return render(request, 'world/zone_detail.html', {'map': this_map})
     pass
