71dfa50b4336a355453fcd063ad750207b029af9
PA Parent <paparent@paparent.me>
2012-11-21 14:19:01 -0500

    First draft for mobile website

--- mobile/urls.py
+++ mobile/urls.py
@@ -2,11 +2,13 @@
 
 from django.conf.urls import patterns, url
 
-from views import home
+from views import home, LocationDetailView, NavigateView
 
 
 urlpatterns = patterns(
     '',
 
     url(r'^$', home, name='mobile_home'),
+    url(r'^location/(?P<slug>[-\w]+)/$', LocationDetailView.as_view(), name='mobile-location-detail'),
+    url(r'^(?P<path>[0-9A-Za-z-_.//]+)/$', NavigateView.as_view(), name='mobile-navigate'),
 )
--- mobile/views.py
+++ mobile/views.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+
+from world.models import Area, Location
 
 
 def home(request):
     return render(request, 'mobile/home.html')
+
+
+class NavigateView(DetailView):
+
+    template_name = 'mobile/navigate.html'
+
+    def get_object(self):
+        return get_object_or_404(Area, path=self.kwargs['path'])
+
+    def get_context_data(self, **kwargs):
+        context = super(NavigateView, self).get_context_data(**kwargs)
+
+        area = self.get_object()
+
+        context['subareas'] = area.get_children()
+        context['locations'] = Location.objects.filter(point__within=area.geom.geom)
+        return context
+
+class LocationDetailView(DetailView):
+
+    model = Location
+    template_name = 'mobile/location_detail.html'
