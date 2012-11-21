569d09f54be39f00acd94d48ac6df6650cbb5aec
PA Parent <paparent@paparent.me>
2012-11-21 14:41:17 -0500

    Mobile: View by location type

--- mobile/urls.py
+++ mobile/urls.py
@@ -2,13 +2,15 @@
 
 from django.conf.urls import patterns, url
 
-from views import home, LocationDetailView, NavigateView
+from views import home, LocationDetailView, LocationTypeListView, LocationTypeDetailView, NavigateView
 
 
 urlpatterns = patterns(
     '',
 
     url(r'^$', home, name='mobile_home'),
+    url(r'^type/$', LocationTypeListView.as_view(), name='mobile-type'),
+    url(r'^type/(?P<pk>\d+)/$', LocationTypeDetailView.as_view(), name='mobile-type-detail'),
     url(r'^location/(?P<slug>[-\w]+)/$', LocationDetailView.as_view(), name='mobile-location-detail'),
     url(r'^(?P<path>[0-9A-Za-z-_.//]+)/$', NavigateView.as_view(), name='mobile-navigate'),
 )
--- mobile/views.py
+++ mobile/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
-from world.models import Area, Location
+from world.models import Area, Location, LocationType
 
 
 def home(request):
@@ -30,3 +30,20 @@ class LocationDetailView(DetailView):
 
     model = Location
     template_name = 'mobile/location_detail.html'
+
+
+class LocationTypeListView(ListView):
+
+    model = LocationType
+    template_name = 'mobile/locationtype_list.html'
+
+
+class LocationTypeDetailView(DetailView):
+
+    model = LocationType
+    template_name = 'mobile/locationtype_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LocationTypeDetailView, self).get_context_data(**kwargs)
+        context['locations'] = Location.objects.filter(type=self.get_object())
+        return context
