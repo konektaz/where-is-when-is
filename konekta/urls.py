526d5fc7cd4b5e0873882cc31d6765f8f4660c18
PA Parent <paparent@paparent.me>
2012-12-11 11:12:47 -0500

    Make the DjangoCMS urls map to the '/'

--- konekta/urls.py
+++ konekta/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from apiv1.api import Api
 from apiv1.resources import AreaResource, LocationResource
 
+from cms.views import details
+
 
 admin.autodiscover()
 
@@ -16,14 +18,11 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'core.views.home', name='home'),
-    url(r'^about/$', 'core.views.about', name='about'),
+    url(r'^$', details, {'slug': ''}, name='pages-root'),
     url(r'^feedback/$', 'core.views.feedback', name='feedback'),
     url(r'^mobile/', include('mobile.urls')),
     (r'^accounts/', include('allauth.urls')),
     url(r'^api/', include(v1_api.urls)),
-
-    url(r'^page/', include('cms.urls')),
 
     url(r'', include('world.urls')),
 )
